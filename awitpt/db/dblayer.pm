14df24596761943a8fbe0a69c74913a6c38789db
Nigel Kukard <nkukard@lbsd.net>
2011-01-13 20:56:04 +0000

    Copyright year update

--- awitpt/db/dblayer.pm
+++ awitpt/db/dblayer.pm
@@ -1,5 +1,5 @@
 # Common database layer module
-# Copyright (C) 2009-2010, AllWorldIT
+# Copyright (C) 2009-2011, AllWorldIT
 # Copyright (C) 2008, LinuxRulz
 # Copyright (C) 2005-2007 Nigel Kukard  <nkukard@lbsd.net>
 #
